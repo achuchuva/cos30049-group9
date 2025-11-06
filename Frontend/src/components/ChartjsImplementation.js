eb40cf479c6afb2e708dc4898025bdacac1ae762
AppelJoosh <joshcauson@gmail.com>
2025-11-07 01:25:00 +1100

    data loading done

--- Frontend/src/components/ChartjsImplementation.js
+++ Frontend/src/components/ChartjsImplementation.js
@@ -1,35 +1,88 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+// import { Doughnut } from 'react-chartjs-2';
+import axios from 'axios'
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+import { useEffect, useState } from 'react';
 
-export const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-        {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-        },
-    ],
-};
+const API_BASE = 'http://localhost:8000';
 
-export default function Chartjs() {
-    return <Doughnut data={data} />;
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// export const data = {
+//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//     datasets: [
+//         {
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)',
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)',
+//             ],
+//             borderWidth: 1,
+//         },
+//     ],
+// };
+
+function Chartjs() {
+  const [stats, setStats] = useState([null]);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+        axios.get(`${API_BASE}/api/v1/stats`)
+            .then((response)=>{
+                if(response.status===200){   
+                    console.log(response.data)
+                    setStats(response.data)
+                }
+                else{
+                    setError("ERROR: "+response.status+" , "+response.statusText)
+                }
+            })
+            .catch((err) => console.log.err);
+    },[]);
+  
+  const data = {
+      labels: ['Total predictions', 'Spam', 'Ham'],
+      datasets: [
+          {
+              label: 'Statistics',
+              data: [stats.total_predictions, stats.spam_count, stats.ham_count],
+              backgroundColor: [
+                  'rgba(168, 168, 168, 0.82)',
+                  'rgba(231, 45, 12, 0.82)',
+                  'rgba(168, 255, 86, 0.6)',
+              ],
+              borderColor: [
+                  'rgba(63, 63, 63, 1)',
+                  'rgba(235, 54, 54, 1)',
+                  'rgba(125, 255, 86, 1)',
+              ],
+              borderWidth: 1,
+          },
+      ],
+  };
+
+  // return <Doughnut data={data} />;
+  return (
+    <section>
+      {stats && (<Bar data={data} />)}
+    </section>
+  );
 }
+
+export default Chartjs;
--- Frontend/src/components/PlotlyImplementation.js
+++ Frontend/src/components/PlotlyImplementation.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import axios from 'axios'
+
+import { useEffect, useState } from 'react';
+
+const API_BASE = 'http://localhost:8000';
 
 export default function PlotlyImplementation() {
+    const [stats, setStats] = useState([null]);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        axios.get(`${API_BASE}/api/v1/stats`)
+            .then((response)=>{
+                if(response.status===200){   
+                    console.log(response.data)
+                    setStats(response.data)
+                }
+                else{
+                    setError("ERROR: "+response.status+" , "+response.statusText)
+                }
+            })
+            .catch((err) => console.log.err);
+    },[]);
+    
     return (
         <Plot
             data={[
@@ -12,9 +34,9 @@ export default function PlotlyImplementation() {
                     mode: 'lines+markers',
                     marker: { color: 'red' },
                 },
-                { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+                { type: 'bar', x: [1, 2, 3], y: [stats.total_predictions, stats.spam_count, stats.ham_count] },
             ]}
-            layout={{ width: 320, height: 240, title: { text: 'A Fancy Plot' } }}
+            layout={{ width: 1000, height: 750, title: { text: 'A Fancy Plot' } }}
         />
     );
 }
